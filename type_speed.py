6c5eb2717f3da3f0b811f6b974243e3d1aaa3ebc
dasFeuer <barunpanthisharma11@gmail.com>
2024-04-29 08:06:39 +0545

    new file added

--- type_speed.py
+++ type_speed.py
@@ -1,28 +1,159 @@
+# import curses
+# from curses import wrapper
+# import time
+# import random
+
+# def startScreen(stdscr):
+#         stdscr.clear()
+#         stdscr.addstr("Welcome to the Speed Typing Test!")
+#         stdscr.addstr("\nPress any key to begin!")
+#         stdscr.refresh()
+#         stdscr.getkey()
+
+# def displayText(stdscr, target, current, wpm=0):
+#     stdscr.addstr(target)
+#     stdscr.addstr(1, 0, f"WPM: {wpm}")
+    
+
+#     for i, char in enumerate(current):
+#         correctChar = target[i]
+#         color = curses.color_pair(1)
+#         if char != correctChar:
+#              color = curses.color_pair(2)
+#         stdscr.addstr(0, i, char, color)
+
+# def loadText():
+#      with open ("text.txt", "r") as f:
+#           lines = f.readlines()
+#           return random.choice(lines).strip()
+
+# def wpmTest(stdscr):
+#       targetText = loadText()
+#       currentText = []
+#       wpm = 0
+#       startTime = time.time()
+#       stdscr.nodelay(True)
+
+#       while True:
+#             timeElapsed = max(time.time() - startTime, 1)
+#             wpm = round((len(currentText) / (timeElapsed / 60)) / 5)
+
+#             stdscr.clear()
+#             displayText(stdscr, targetText, currentText, wpm)
+
+#             stdscr.refresh()
+            
+#             if "".join(currentText) == targetText:
+#                 stdscr.nodelay(False)
+#                 break
+
+#             try:
+#                 key = stdscr.getkey()
+#             except:
+#                 continue
+
+#             if ord(key) == 27:
+#                 break
+
+#             if key in ("KEY_BACKSPACE", '\b', "\x7f"):
+#                 if len(currentText) > 0:
+#                       currentText.pop()
+#                 elif len(currentText) < len(targetText):
+#                     currentText.append(key)
+
+# def main(stdscr):
+#     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
+#     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
+#     curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
+
+#     startScreen(stdscr)
+
+#     while True:
+#         wpmTest(stdscr)
+#         stdscr.addstr(2, 0, "You completed the text! Press any key to continue...")
+#         key = stdscr.getkey()
+
+#         if ord(key) == 27:
+#             break
+
+# wrapper(main)
+
 import curses
 from curses import wrapper
+import time
+import random
 
-def startScreen(stdscr):
-        stdscr.clear()
-        stdscr.addstr("Welcome to the Speed Typing Test!")
-        stdscr.addstr("\nPress any key to begin!")
-        stdscr.refresh()
-        stdscr.getkey()
 
-def wpmTest(stdscr):
-      targetText = "Hello World this some test text for this app!"
-      currentText = []
-      stdscr.clear()
-      stdscr.addstr(targetText)
-      stdscr.refresh()
-      
+def start_screen(stdscr):
+	stdscr.clear()
+	stdscr.addstr("Welcome to the Speed Typing Test!")
+	stdscr.addstr("\nPress any key to begin!")
+	stdscr.refresh()
+	stdscr.getkey()
+
+def display_text(stdscr, target, current, wpm=0):
+	stdscr.addstr(target)
+	stdscr.addstr(1, 0, f"WPM: {wpm}")
+
+	for i, char in enumerate(current):
+		correct_char = target[i]
+		color = curses.color_pair(1)
+		if char != correct_char:
+			color = curses.color_pair(2)
+
+		stdscr.addstr(0, i, char, color)
+
+def load_text():
+	with open("text.txt", "r") as f:
+		lines = f.readlines()
+		return random.choice(lines).strip()
+
+def wpm_test(stdscr):
+	target_text = load_text()
+	current_text = []
+	wpm = 0
+	start_time = time.time()
+	stdscr.nodelay(True)
+
+	while True:
+		time_elapsed = max(time.time() - start_time, 1)
+		wpm = round((len(current_text) / (time_elapsed / 60)) / 5)
+
+		stdscr.clear()
+		display_text(stdscr, target_text, current_text, wpm)
+		stdscr.refresh()
+
+		if "".join(current_text) == target_text:
+			stdscr.nodelay(False)
+			break
+
+		try:
+			key = stdscr.getkey()
+		except:
+			continue
+
+		if ord(key) == 27:
+			break
+
+		if key in ("KEY_BACKSPACE", '\b', "\x7f"):
+			if len(current_text) > 0:
+				current_text.pop()
+		elif len(current_text) < len(target_text):
+			current_text.append(key)
+
 
 def main(stdscr):
-    curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
-    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
-    curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
+	curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
+	curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
+	curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
 
-    startScreen(stdscr)
-    wpmTest(stdscr)
+	start_screen(stdscr)
+	while True:
+		wpm_test(stdscr)
+		stdscr.addstr(2, 0, "You completed the text! Press any key to continue...")
+		key = stdscr.getkey()
+		
+		if ord(key) == 27:
+			break
 
 wrapper(main)
-
